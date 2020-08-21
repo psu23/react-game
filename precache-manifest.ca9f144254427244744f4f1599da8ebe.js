ff9d903c3f589640f0fa4f51cb180e6c6e1c6819
Patrick Urbankowski <purbankowski@gmail.com>
2020-08-21 16:06:09 -0400

    Updates

---
+++ precache-manifest.ca9f144254427244744f4f1599da8ebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4472a3209141c1e27cb5687df2634775",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "95a72d2f03b019ee154c",
+    "url": "/react-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae00459dc451066c22ae",
+    "url": "/react-game/static/js/2.dbc5170f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-game/static/js/2.dbc5170f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a72d2f03b019ee154c",
+    "url": "/react-game/static/js/main.0bf0ce60.chunk.js"
+  },
+  {
+    "revision": "97bb4915e77e8e10ea2d",
+    "url": "/react-game/static/js/runtime-main.5660c68a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
